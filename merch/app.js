912e3dd33cdb58d7982400aee0a8f9f99da6ab80
Rohith Chalasani <rohithchalasani88@gmail.com>
2026-08-03 19:54:40 +0530

    Guest checkout direct redirect

--- merch/app.js
+++ merch/app.js
@@ -4318,7 +4318,7 @@
       return;
     }
 
-    openGuestCheckoutModal();
+    showCheckoutPage({ name: '', email: '', phone: '' }, { country: 'India' });
   }
 
   async function startRazorpayCheckout(customer, address) {
